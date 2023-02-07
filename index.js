5f1994adf12201c4ea797d1b32c3ae333b734ea3
Vince <65380728+Socerest2@users.noreply.github.com>
2023-02-07 18:23:09 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -7,4 +7,16 @@ function updateCon() {
   // Create a parser using the createElement() method
   var parser = document.createElement("a");
   parser.href = url;
+
+  fetch(url)
+    .then(response => response.text())
+    .then(htmlContent => {
+      var parser = new DOMParser();
+      var htmlDoc = parser.parseFromString(htmlContent, "text/html");
+
+      // Find a specific element in the HTML
+      var desiredElement = htmlDoc.getElementById("some-element-id");
+      console.log(desiredElement.innerHTML);
+    })
+    .catch(error => console.error(error));
 }
